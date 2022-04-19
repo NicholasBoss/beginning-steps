b3557fb6f9645622722dea2966948c178fd606e8
NicholasBoss <nicholasboss0914@gmail.com>
2022-04-19 13:27:16 -0600

    first commit

---
+++ helloworld-C++/helloworld/helloworld.cpp
@@ -0,0 +1,11 @@
+// helloworld.cpp 
+// Created by Nicholas Boss 4/19/22
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n"; // prints out 'Hello World' in C++ language
+}
+
---
+++ helloworld-python/helloworld.py
@@ -0,0 +1,1 @@
+print("Hello World") # prints Hello World in python language
